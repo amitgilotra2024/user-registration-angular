c315bd76ccba7bd6a48f6cb8512d6a5fb8175435
amitgilotra2024 <155868329+amitgilotra2024@users.noreply.github.com>
2024-01-08 13:28:30 -0500

    Update register.component.ts

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,17 +1,16 @@
 
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { User } from '../../model/user';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'register',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RegisterComponent],
-  templateUrl: './register.component.html',
+    templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
@@ -22,31 +21,41 @@ export class RegisterComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private httpClient: HttpClient
-    ) {}
-
-
-  ngOnInit(): void {
+  ) {
     this.registerForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       postalCode: ['', [Validators.required, Validators.pattern('^[A-Za-z]\\d[A-Za-z][ -]?\\d[A-Za-z]\\d$')]]
     });
+   }
+
+
+  ngOnInit(): void {
+
   }
 
-  onSubmit() : void {
+  onSubmit(): void {
+    console.log('registerForm.invalid ',this.registerForm.invalid)
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
     const user = new User();
     const hasValues = this.registerForm.value.firstName && this.registerForm.value.lastName && this.registerForm.value.email && this.registerForm.value.postalCode;
-    if(hasValues) {
+
+    if (hasValues) {
       user.firstName = this.registerForm.value.firstName;
       user.lastName = this.registerForm.value.lastName;
       user.email = this.registerForm.value.email;
       user.postalCode = this.registerForm.value.postalCode;
       if (this.registerForm.valid) {
-          const url = `http://localhost:8080/users/addUser`;
-          this.httpClient.post(url, user).subscribe();
-        }
-    }
+        const url = `http://localhost:8080/users/addUser`;
+        this.httpClient.post(url, user).subscribe();
+      }
     }
   }
+
+}
 
