1a68655669adf7d22671beb3b092b04f6d0ea5b3
Amit Gilotra <emailtoamitg@gmail.com>
2024-01-09 12:55:02 -0500

    User registration code changes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,16 @@
 
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { UserserviceService } from '../../service/userservice.service';
-import { User } from '../../model/user';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { User } from '../../model/user';
+import { SharedModule } from '../../shared.module';
 
 @Component({
   selector: 'register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RegisterComponent, SharedModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -17,14 +18,14 @@ export class RegisterComponent implements OnInit {
 
   registerForm!: FormGroup;
   user!: User;
+  defaultLanguage = 'en';
 
   constructor(
     private formBuilder: FormBuilder,
-    private userService: UserserviceService,
-    ) {}
-
-
-  ngOnInit(): void {
+    private httpClient: HttpClient,
+    private translateService: TranslateService
+  ) {
+    translateService.setDefaultLang(this.defaultLanguage);
     this.registerForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -33,28 +34,37 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  onSubmit() : void {
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    console.log('registerForm.invalid ', this.registerForm.invalid)
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
     const user = new User();
     const hasValues = this.registerForm.value.firstName && this.registerForm.value.lastName && this.registerForm.value.email && this.registerForm.value.postalCode;
-    if(hasValues) {
+
+    if (hasValues) {
       user.firstName = this.registerForm.value.firstName;
       user.lastName = this.registerForm.value.lastName;
       user.email = this.registerForm.value.email;
       user.postalCode = this.registerForm.value.postalCode;
 
       if (this.registerForm.valid) {
-        this.userService.addUser(user).subscribe({
-         next: (res: User) => {
-            console.log(res);
-            alert('Registration successful');
-          },
-          error: (err) => {
-            console.log(err);
-            alert('Registration failed');
-          }
-     });
-    }
+        this.registerForm.reset();
+        const url = `http://localhost:8080/users/addUser`;
+        this.httpClient.post(url, user).subscribe();
+      }
     }
   }
+
+  changeLanguage(): void {
+    this.defaultLanguage = this.defaultLanguage === 'en' ? 'fr' : 'en'
+    this.translateService.setDefaultLang(this.defaultLanguage);
+  }
+
 }
 
